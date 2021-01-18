4555d420a444b8e795bc2ea26756225c34a842d5
ConnorMul <mulhollandconnor@icloud.com>
2021-01-18 13:42:26 -0500

    core delivs complete

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([])
+  const [search, setSearch] = useState("")
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+    .then(resp => resp.json())
+    .then(plantObjs => setPlants(plantObjs))
+  }, [])
+
+  const searchedPlants = plants.filter((plant) =>
+    plant.name.toLowerCase().includes(search.toLowerCase())
+  )
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm plants={searchedPlants} onAddPlant={setPlants} />
+      <Search search={search} onSearch={setSearch}/>
+      <PlantList plants={searchedPlants} />
     </main>
   );
 }
